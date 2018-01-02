8e92de0a7804bfdd30ef05ce5d21f82a87fcd9e4
dishonkuria <amster2ent@gmail.com>
2018-01-02 14:09:46 +0300

    add variable for both player names

--- js/scripts.js
+++ js/scripts.js
@@ -68,6 +68,14 @@ Player.prototype.winnerCheck = function () {
      $(".player-console").show();
      $(".start-menu").hide();
 
+     var player1Name = $(".player1 Name").val();
+     $("#player1Name").text(player1Name);
+
+     var player2Name =$(".player2Name").val();
+     $("#player2Name").text(player2Name);
+
+     player1.playerName=player1Name;
+     player2.playerName=player2Name;
      
    }
  }
